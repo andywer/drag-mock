7407c7b2f8bd4763a6fade84fc168498472e0e04
Andy Wermke <andy@dev.next-step-software.com>
2015-02-06 16:46:33 +0100

    Extend test case

--- test/spec/dragMock.spec.js
+++ test/spec/dragMock.spec.js
@@ -3,30 +3,47 @@
 (function() {
   describe('drag-mock', function() {
 
-    var DragDropAction = dragMock.DragDropAction;
-
     var elementA = document.querySelector('#dom-test > .a')
-      , elementB = document.querySelector('#dom-test > .b');
+      , elementB = document.querySelector('#dom-test > .b')
+      , elementC = document.querySelector('#dom-test > .c');
 
     it('can drag and drop', function(done) {
 
-      var dropListener;
+      var bListener, bDone = false
+        , cListener, cDone = false;
 
-      elementB.addEventListener('drop', dropListener = function(event) {
+      function listenersRun() {
+        if (bDone && cDone) { done(); }
+      }
+
+      elementB.addEventListener('drop', bListener = function(event) {
         expect(event.type).to.equal('drop');
         expect(event.clientX).to.equal(100);
         expect(event.clientY).to.equal(200);
         expect(event.dataTransfer.getData('text')).to.equal('Drop with care!');
 
-        elementB.removeEventListener('drop', dropListener);
-        done();
+        elementB.removeEventListener('drop', bListener);
+        bDone = true;
+        listenersRun();
+      });
+
+      elementC.addEventListener('drop', cListener = function(event) {
+        expect(event.type).to.equal('drop');
+        expect(event.clientX).to.equal(500);
+        expect(event.clientY).to.equal(400);
+        expect(event.dataTransfer.getData('text')).to.equal('Drop with care!');
+
+        elementC.removeEventListener('drop', cListener);
+        cDone = true;
+        listenersRun();
       });
 
       dragMock
         .dragStart(elementA, function(event) {
           event.dataTransfer.setData('text', 'Drop with care!');
         })
-        .drop(elementB, { clientX: 100, clientY: 200 });
+        .drop(elementB, { clientX: 100, clientY: 200 })
+        .drop(elementC, { clientX: 500, clientY: 400 });
 
     });
 
